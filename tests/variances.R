fadf2bfda42ecad50205fb7f65cc4db215b72729
nashjc <nashjc@uottawa.ca>
2017-04-29 23:00:42 -0400

    fix formulas

--- tests/variances.R
+++ tests/variances.R
@@ -33,7 +33,38 @@
       shift <- 10 * shift
       ds <- ds0 + shift
       cat(fmla," ",var(ds),"\n")
-      readline("cont.")
+  #    readline("cont.")
   }
   cat("failed for shift =",shift,"\n")
   
+  cat("geometric progressions\n")
+  a <- 1
+  r <- 1.1
+  n <- 100
+  sq <- 0:(n-1)
+  gp <- a*(r^sq)
+  gp
+  sumgp <- a*(1 - r^n)/(1-r)
+  sumgp
+  sum(gp)
+  gp2 <- a*a*(r^(2*sq))
+  gp2
+  sum(gp2)
+  sumgp2 <- a*a*(1 - r^(2*n))/(1 - r*r)
+  sumgp2
+  var(gp) 
+mean(gp)
+sumgp/n
+sum(gp^2)  
+# a*a*(1 - (r^2)^n)/(1-r^2)
+
+vgp <- 2*(r^n-2*r)*a*a*n*(1-r^n)/((n-1)*(1-r*r)*(1+r))
+vgp
+var(gp)
+
+# (r^n + 1)/(r+1) - (r^n-1)/(r-1)
+
+vgp2 <- a*a*(1-r^n)*((1+r^n)/(1+r)-(1-r^n)/(n*(1-r)))/((n-1)*(1-r))
+vgp2
+var(gp)
+# vgp2a <- (a*a*(1-r)/())
